d7a9e49f08e01dd0aa8fbe4ba6b0e3ae21d6232a
Serra KIR DWORCZAK <serrakir@gmail.com>
2024-03-05 15:47:04 +0000

    Add code to get random questions from questions array

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,16 +21,24 @@ const menuBox = document.getElementById('menu-box');
 // to make game area visible & hiding menu buttons https://www.w3schools.com/jsref/met_document_queryselector.asp
 const gameArea = document.querySelector('.game-area');
 
+let randomQuestion, presentQuestion
+
 // Starts the game & hides the start and how to buttons
 function startGame() {
     menuBox.classList.add("hide");
     gameArea.classList.remove("hide");
+    randomQuestion = questions.sort(() => Math.random() - 0.5);
+    presentQuestion = 0;
+    console.log(randomQuestion)
+    getNextQuestion();
 
 };
 
 // Gets to next question automatically when picked an answer
 function selectAnswer() {
-    // function getNextQuestion() {}
+    // when answer is selected, move to to next question
+    getNextQuestion();
+
 };
 
 // create questions
