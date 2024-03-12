bbc4885882c9c1f73c16f31e1d463bdb1aa60f13
Serra KIR DWORCZAK <serrakir@gmail.com>
2024-03-12 12:02:18 +0000

    Organize code and add missing semicolons in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,28 @@ const closeDialog = document.getElementById('btn-dialog-off');
 const dialog = document.getElementById('dialog-box');
 const menuBox = document.getElementById('menu-box');
 const questionElm = document.getElementById('question-box');
-
 // to make game area visible & hiding menu buttons https://www.w3schools.com/jsref/met_document_queryselector.asp
 const gameArea = document.querySelector('.game-area');
 
 let randomQuestion, presentQuestion;
+const gameTimer = 15000;
+let timer;
+// count scores
+let correctScore = 0;
+let incorrectScore = 0;
+const correctScoreElm = document.querySelector('#correct');
+const incorrectScoreElm = document.querySelector('#incorrect');
+
+// create questions
+const questions = [{
+    question: 'Yellow',
+}, {
+    question: 'Blue',
+}, {
+    question: 'Green',
+}, {
+    question: 'Red',
+}];
 
 // Event listener for Start button
 startButton.addEventListener('click', startGame);
@@ -26,8 +43,6 @@ closeDialog.addEventListener('click', () => {
     dialog.close();
 });
 
-const gameTimer = 15000;
-let timer;
 
 // Starts the game & hides the start and how to buttons
 function startGame() {
@@ -59,8 +74,8 @@ function startGame() {
                 selectAnswer();
             });
             selectAnswer();
-        })
-    })
+        });
+    });
 };
 
 // * get the next color question
@@ -85,23 +100,16 @@ function selectAnswer() {
         clearInterval(timer);
         gameArea.classList.add("hide");
         const gameOver = document.createElement('p');
-        gameOver.innerText = 'Game Over'
+        gameOver.innerText = 'Game Over';
         document.body.appendChild(gameOver);
         gameOver.style.color = '#ffffff';
         // template strings to show user their score
         const finalScore = document.createElement('p');
-        finalScore.innerText = `Your Score is: ${correctScore}`
+        finalScore.innerText = `Your Score is: ${correctScore}`;
         document.body.appendChild(finalScore);
         finalScore.style.color = '#ffffff';
     }
 };
-
-// count scores
-let correctScore = 0;
-let incorrectScore = 0;
-
-const correctScoreElm = document.querySelector('#correct')
-const incorrectScoreElm = document.querySelector('#incorrect')
 
 // check answers 
 function checkCorrectAnswer(question, selectedAnswer) {
@@ -118,15 +126,3 @@ function checkCorrectAnswer(question, selectedAnswer) {
     }
 
 }
-
-
-// create questions
-const questions = [{
-    question: 'Yellow',
-}, {
-    question: 'Blue',
-}, {
-    question: 'Green',
-}, {
-    question: 'Red',
-}];
